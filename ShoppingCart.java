3c030ca0b623630e88d7e5aa82f387defb052015
valente1028 <97548063+valente1028@users.noreply.github.com>
2023-12-09 12:42:54 -0500

    ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -7,82 +7,92 @@ package com.mycompany.shoppingcart;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import javax.swing.JOptionPane;
 
-/**
- * ShoppingCart class represents the shopping cart functionality.
- */
 public class ShoppingCart {
 
-    private ArrayList<Product> items = new ArrayList<Product>();
+    private ArrayList<Product> items = new ArrayList<>();
 
-    /**
-     * Display all products in the shopping cart.
-     */
-    public void showAllProducts(Product products[]) {
+    public ShoppingCart() {
+        // Empty constructor
+    }
+     public void showAllProducts() {
         System.out.println("Shopping Cart Contents:");
         System.out.printf("%-5s %-15s %-10s %-8s\n", "ID", "Name", "Price", "Quantity");
 
-        for (Product p : products) {
+        for (Product p : items) {
             System.out.printf("%-5d %-15s %-10.2f %-8d\n", p.getId(), p.getName(), p.getPrice(), p.getQuantity());
         }
         System.out.println();
     }
 
-    /**
-     * Add a product to the shopping cart.
-     *
-     * @param product  Product to add.
-     * @param quantity Quantity of the product to add.
-     */
     public void addProduct(Product product, int quantity) {
         if (product != null && product.getQuantity() >= quantity) {
-            product.setQuantity(product.getQuantity() - quantity);
             items.add(new Product(product.getId(), product.getName(), product.getPrice(), quantity));
             System.out.println("Product added successfully");
         } else if (product != null) {
             System.out.println("Quantity is not present in the stock");
-        } else {
-            System.out.println("Product not found");
         }
         System.out.println();
     }
 
-    /**
-     * Remove a product from the shopping cart.
-     *
-     * @param id ID of the product to remove.
-     * @param products Array of available products.
-     */
-    public void removeProduct(int id, Product products[]) {
-        Product removedProduct = null;
-        for (Product product : items) {
-            if (product.getId() == id) {
-                removedProduct = product;
+    public void removeProduct(int productId, Product[] products) {
+        for (Product item : items) {
+            if (item.getId() == productId) {
+                items.remove(item);
+                // Restock the product in the products array
+                for (Product product : products) {
+                    if (product.getId() == productId) {
+                        product.setQuantity(product.getQuantity() + item.getQuantity());
+                        break;
+                    }
+                }
+                System.out.println("Product deleted successfully");
                 break;
             }
         }
-
-        if (removedProduct != null) {
-            items.remove(removedProduct);
-            updateProductQuantity(products, id, removedProduct.getQuantity());
-            System.out.println("Product removed from the cart");
-        } else {
-            System.out.println("Product not found in the cart");
-        }
-
         System.out.println();
     }
 
-    // Helper method to update product quantity in the products array
-    private void updateProductQuantity(Product products[], int id, int quantity) {
-        for (Product product : products) {
-            if (product.getId() == id) {
-                product.setQuantity(product.getQuantity() + quantity);
+    public void editProduct(int productId, int newQuantity) {
+        for (Product item : items) {
+            if (item.getId() == productId) {
+                if (item.getQuantity() >= newQuantity) {
+                    item.setQuantity(newQuantity);
+                    System.out.println("Product edited successfully");
+                } else {
+                    System.out.println("Quantity is not present in the stock");
+                }
                 break;
             }
         }
+        System.out.println();
     }
 
-    // ... (other methods remain unchanged)
+    public void showAllProducts(Product[] products) {
+        System.out.println("Shopping Cart Contents:");
+        System.out.printf("%-5s %-15s %-10s %-8s\n", "ID", "Name", "Price", "Quantity");
+
+        for (Product p : items) {
+            System.out.printf("%-5d %-15s %-10.2f %-8d\n", p.getId(), p.getName(), p.getPrice(), p.getQuantity());
+        }
+        System.out.println();
+    }
+
+    public void totalPrice(Product[] products) {
+        double total = 0;
+        for (Product item : items) {
+            for (Product product : products) {
+                if (item.getId() == product.getId()) {
+                    total += (product.getPrice() * item.getQuantity());
+                }
+            }
+        }
+        System.out.println("Your total bill is : " + total);
+        System.out.println();
+    }
+
+    public void confirmOrder() {
+        System.out.println("Order confirmed. Thank you for shopping!");
+    }
 }
+
